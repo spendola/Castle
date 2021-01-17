67e8b8429cd224abe9ff0e4c97323ad80a99c525
Sebastian Pendola <sebastianpendola@Sebastians-MacBook-Pro.local>
2021-01-17 14:58:38 -0500

    initial implementation as service

--- Castle.py
+++ Castle.py
@@ -1,17 +1,35 @@
-import os
+#!flask/bin/python
+from flask import Flask
+from flask import request
+from flask import jsonify
+import PIL.Image
+import numpy as np
+import pymysql
+import zlib
 import json
-from openalpr import Alpr
+import io
+import gzip
+import PlateRecognition as PlateRecognition
+
+
+# Service Definition
+app = Flask(__name__)
+@app.route('/', methods = ['POST'])
 
 def main():
-	print("Castle Server")
-
-	alpr = Alpr("us", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
-	if not alpr.is_loaded():
-		print("Error loading OpenALPR")
-		sys.exit(1)
-	results = alpr.recognize_file("/path/to/image.jpg")
-	print(json.dumps(results, indent=4))
-	alpr.unload()
 	
-if __name__ == "__main__": 
-	main()
+	try:
+		data = request.get_data()
+		data = bytearray(data)
+		data = np.asarray(data, dtype=np.uint8)
+		data = data.reshape(800, 600)
+		return "good_image"
+	except:
+		return "bad_image_shape"
+		
+	
+
+if __name__ == "__main__":
+	print("Initializing Castle")
+	Preload()
+	app.run(host="0.0.0.0", port=9000)
