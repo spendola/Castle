c53e09791cd6c497f12eb9a375050348d9b6e0c0
Sebastian Pendola <sebastianpendola@Sebastians-MacBook-Pro.local>
2021-01-18 16:08:13 -0500

    changes home directory

--- FtpService.py
+++ FtpService.py
@@ -48,7 +48,7 @@ def main():
 
 	# Define a new user having full r/w permissions and a read-only
 	# anonymous user
-	authorizer.add_user('intesla', 'cata', '/home/ftp/', perm='elradfmwMT')
+	authorizer.add_user('intesla', 'cata', '.', perm='elradfmwMT')
 	#authorizer.add_anonymous(os.getcwd())
 
 	# Instantiate FTP handler class
