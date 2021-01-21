980e9391702f9cb5c43ab5a953e01ff51ef83b86
sebastian <sebastian@pendola.net>
2021-01-20 22:44:40 -0500

    changes on listen

--- FtpCallback.py
+++ FtpCallback.py
@@ -2,13 +2,12 @@
 import subprocess
 import sys
 
-
-print("Python received: " + sys.argv[1])
 line = sys.argv[1]
 start = line.find("/files")
 end = line.find(",", start)
 filename = "/home/castle/ftp" + line[start:end-1]
-print(filename)
 
-output = subprocess.check_output("alpr " + filename)
+print(filename)
+output = str(subprocess.check_output("sudo alpr " + filename, shell=True)).replace("\\t", " ").replace("\\n", ";")
 print(output)
+
--- listenFtp.sh
+++ listenFtp.sh
@@ -3,10 +3,7 @@
 SUB='OK UPLOAD'
 tail -F /var/log/vsftpd.log | while read line; do
   if [[ "$line" == *"$SUB"* ]]; then
-    echo "found"
-    filename=$(echo "$line" | cut -d, -f2)
-    if [ -s "$filename" ]; then
-      echo "$filename was received"
-    fi
+    msg=${line//[ ]/_}
+    python3 FtpCallback.py $msg
   fi
 done
