ff7b402cbb64fdfd66f3c2563b425eb18ca8b1ff
Sebastian Pendola <sebastianpendola@Sebastians-MacBook-Pro.local>
2021-01-20 22:16:38 -0500

    process line

--- FtpCallback.py
+++ FtpCallback.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
+import subprocess
 import sys
 
+
 print("Python received: " + sys.argv[1])
+line = sys.argv[1]
+start = line.find("/files")
+end = line.find(",", start)
+filename = line[start:end]
+print(filename)
