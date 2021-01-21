59f5f81de09d1921aab951401692a77be3584a65
Sebastian Pendola <sebastianpendola@Sebastians-MacBook-Pro.local>
2021-01-20 22:21:18 -0500

    adds subprocess call

--- FtpCallback.py
+++ FtpCallback.py
@@ -7,5 +7,8 @@ print("Python received: " + sys.argv[1])
 line = sys.argv[1]
 start = line.find("/files")
 end = line.find(",", start)
-filename = line[start:end]
+filename = "/home/castle/ftp" + line[start:end-1]
 print(filename)
+
+output = subprocess.check_output("alpr " + filename)
+print(output)
