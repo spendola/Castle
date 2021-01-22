f5c428d632b7e133a2704aa6b01a9d2d83c6bfcb
sebastian <sebastian@pendola.net>
2021-01-21 22:50:13 -0500

    updates model

--- Castle.py
+++ Castle.py
@@ -10,13 +10,13 @@ from sh import tail
 
 # initialization of imageai
 localpath = os.path.dirname(os.path.realpath(__file__))
-modelpath = os.path.join(self.localpath, "models/resnet50_coco_best_v2.0.1.h5")
+modelpath = os.path.join(localpath, "models/resnet50_coco_best_v2.1.0.h5")
 
 detector = imageai.Detection.ObjectDetection()
 detector.setModelTypeAsRetinaNet()
-detector.setModelPath(self.modelpath)
-detector.loadModel(detection_speed="fast")
-custom_objects = detector.CustomObjects(person=True, sports_ball=True)
+detector.setModelPath(modelpath)
+detector.loadModel()
+#custom_objects = detector.CustomObjects(person=True, sports_ball=True)
 		
 		
 for line in tail ("-f", "/var/log/vsftpd.log", _iter=True):
@@ -26,12 +26,16 @@ for line in tail ("-f", "/var/log/vsftpd.log", _iter=True):
 		filename = "/home/castle/ftp" + line[start:end-1]
 
 		print(filename)
-		output = str(subprocess.check_output("sudo alpr " + filename, shell=True)).replace("\\t", " ").replace("\\n", ";")
-		print(output)
+		try:
+			output = str(subprocess.check_output("sudo alpr " + filename, shell=True)).replace("\\t", " ").replace("\\n", ";")
+			print(output)
 
-		if("No license plates found" not in output):
-			context = ssl._create_unverified_context()
-			post_data = urllib.parse.urlencode({"owner":"intesla_test", "activity":output}).encode('utf-8')
-			x = urllib.request.urlopen(url="http://www.pendola.net/api/castleapi.php", data=post_data, context=context, timeout=15)
-			html = x.read().decode("utf-8")
-			print(html)
+			if("No license plates found" not in output):
+				context = ssl._create_unverified_context()
+				post_data = urllib.parse.urlencode({"owner":"intesla_test", "activity":output}).encode('utf-8')
+				x = urllib.request.urlopen(url="http://www.pendola.net/api/castleapi.php", data=post_data, context=context, timeout=15)
+				html = x.read().decode("utf-8")
+				print(html)
+		except:
+			print("ouch... that wasn't an image")
+			pass
